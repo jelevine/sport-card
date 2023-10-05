1bb853b99cf18d09174d3ee8ab7e823a467cfacd
jelevine <jelevine21@gmail.com>
2023-10-05 13:19:25 -0400

    new javascript

--- src/sport-card.js
+++ src/sport-card.js
@@ -4,13 +4,18 @@ const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 
 class SportCard extends LitElement {
   static properties = {
-    header: { type: String },
+    image: { type: String },
+    alt: { type: String },
+    title: {type: String },
+    description: { type: String },
+    detailButtonText: { type: String}
   }
 
   static styles = css`
       .card {
   display: flex;
   justify-content: space-evenly;
+  flex-wrap: wrap;
 }
 .card-container {
   background-color: #041E42;
@@ -56,21 +61,13 @@ img {
   border-radius: 5px;
 }
 
-.card-content button{
-  margin-top: 20px;
-  margin-bottom: 20px;
-  text-decoration: none;
-  background-color: #ffff;
-  color: #041E42;
-  padding: 8px 12px;
-  border-radius: 5px;
-}
-
-.card-content button:active,
-.card-content button:focus,
-.card-content button:hover{
+button:active,
+button:focus,
+button:hover{
   background-color: #041E42;
-  color: white;   
+  color: white;
+  transition: background-color 0.2s;
+  transition: color 0.2s;   
 }
 
 .card-content button a{
@@ -108,7 +105,8 @@ img {
 .card-content #toggleDescription:hover{
   background-color: #041E42;
   color: white;   
-  transition: background-color 0.3s;
+  transition: background-color 0.2s;
+  transition: color 0.2s;
 }
 
 @media (max-width: 800px) and (min-width: 500px){
@@ -130,63 +128,11 @@ img {
 
   constructor() {
     super();
-    this.header = 'My app';
-  }
- 
-  firstUpdate(){
-   const cloneButton = this.shadowRoot.querySelector("#clone");
-   const deleteButton = this.shadowRoot.querySelector('#delete');
-   const backgroundButton = this.shadowRoot.querySelector('#background');
-   const writingButton = this.shadowRoot.querySelector('#writing');
-   
-   cloneButton.addEventListener('mousemove', (e) => {
-    cloneButton.style.backgroundColor = "#041E42";
-    cloneButton.style.color = 'white';
-   });
-   cloneButton.addEventListener('focus', (e) => {
-    cloneButton.style.backgroundColor = "#041E42";
-    cloneButton.style.color = 'white';
-   });
-   cloneButton.addEventListener('mouseout', (e) => {
-    cloneButton.style.backgroundColor = '';
-    cloneButton.style.color = "";
-    })
-   deleteButton.addEventListener('mousemove', (e) => {
-    deleteButton.style.backgroundColor = "#041E42";
-    deleteButton.style.color = 'white';
-   });
-   deleteButton.addEventListener('focus', (e) => {
-    deleteButton.style.backgroundColor = "#041E42";
-    deleteButton.style.color = 'white';
-   });
-  deleteButton.addEventListener('mouseout', (e) => {
-    deleteButton.style.backgroundColor = '';
-    deleteButton.style.color = "";
-   });
-   backgroundButton.addEventListener('mousemove', (e) => {
-    backgroundButton.style.backgroundColor = "#041E42";
-    backgroundButton.style.color = 'white';
-   });
-   backgroundButton.addEventListener('focus', (e) => {
-    backgroundButton.style.backgroundColor = "#041E42";
-    backgroundButton.style.color = 'white';
-   });
-  backgroundButton.addEventListener('mouseout', (e) => {
-    backgroundButton.style.backgroundColor = '';
-    backgroundButton.style.color = "";
-   });
-   writingButton.addEventListener('mousemove', (e) => {
-    writingButton.style.backgroundColor = "#041E42";
-    writingButton.style.color = 'white';
-   });
-   writingButton.addEventListener('focus', (e) => {
-    writingButton.style.backgroundColor = "#041E42";
-    writingButton.style.color = 'white';
-   });
-  writingButton.addEventListener('mouseout', (e) => {
-    writingButton.style.backgroundColor = '';
-    writingButton.style.color = "";
-   });
+    this.image = "https://dgiqkglfef83i.cloudfront.net/images/2023/4/15/Blue-White.png";
+    this.alt = "B+W Game Image";
+    this.title = "Penn State Blue and White Game";
+    this.description = "With all hype on the upcoming football season, the Blue and White game gives the fans something to look forward to watching in the spring!"
+    this.detailButtonText = "Description";
   }
 
   cloneCard(e){
@@ -202,7 +148,23 @@ img {
     clone.addEventListener('mouseout', () => {
         clone.style.backgroundColor = '#041E42'
     })
+
+    const changeTitle = this.shadowRoot.querySelector('#writing');
+    changeTitle.addEventListener('click', () => {
+      const title = clone.querySelector('h2');
+      if (title.innerHTML == 'something else'){
+        title.innerHTML = 'Penn State Blue and White Game';
+      }
+      else{
+        title.innerHTML = 'something else';
+      }
+    })
     
+    const changeBackground = this.shadowRoot.querySelector('#background');
+    changeBackground.addEventListener('click', () => {
+      const color = Math.floor(Math.random() * 16777215).toString(16);
+      clone.style.backgroundColor = '#' + color;    
+    })
 
     this.shadowRoot.querySelector('.card').appendChild(clone);
 
@@ -259,11 +221,11 @@ img {
       @mouseover="${this.cardHover}"
       @mouseout="${this.cardHover}">
       <div class = "card-content">
-        <h2 id = 'title'>Penn State Blue and White Game</h2>
-        <img align = "center" src = "https://dgiqkglfef83i.cloudfront.net/images/2023/4/15/Blue-White.png" alt = "B+W Game Image">
+        <h2 id = 'title'>${this.title}</h2>
+        <img align = "center" src = "${this.image}" alt = "${this.alt}">
         <details>
-          <summary id = "toggleDescription">Description</summary>
-          <p id = 'cDescription'>With all hype on the upcoming football season, the Blue and White game gives the fans something to look forward to watching in the spring!</p>
+          <summary id = "toggleDescription">${this.detailButtonText}</summary>
+          <p id = 'cDescription'>${this.description}</p>
         </details>
       </div>
     </div>
